27833c2249eb3368dbbfb18a5252f546a1bbda65
Shohruxbek <email@manzil.com>
2025-04-20 13:35:46 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,6 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
--- main/serializers.py
+++ main/serializers.py
@@ -1,46 +1,72 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
-
-from .models import Teacher, Class, Student
+from .models import User, Teacher, Student, Class
 
 
-class StudentSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = "__all__"
 
-
-class ClassSerializers(serializers.ModelSerializer):
-    class_name = StudentSerializers(many=True)
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = Class
-        fields = "__all__"
+        model = User
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'role']
 
     def create(self, validated_data):
-        class_name = validated_data.pop('class_name')
-        obj = Class.objects.create(**validated_data)
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
 
-        for i in class_name:
-            Student.objects.create(obj=obj, **i)
 
-        return obj
+class TeacherSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Teacher
+        fields = ['user', 'phone_number', 'address', 'photo', 'experience']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create_user(**user_data)
+        teacher = Teacher.objects.create(user=user, **validated_data)
+        return teacher
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    cla_ss_string = serializers.StringRelatedField(source='cla_ss')
+    cla_ss_pk = serializers.PrimaryKeyRelatedField(queryset=Class.objects.all(), source='cla_ss')
+    cla_ss_hyperlink = serializers.HyperlinkedRelatedField(
+        view_name='class-detail', queryset=Class.objects.all(), source='cla_ss'
+    )
+    cla_ss_nested = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Student
+        fields = ['user', 'full_name', 'cla_ss_string', 'cla_ss_pk', 'cla_ss_hyperlink', 'cla_ss_nested']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create_user(**user_data)
+        student = Student.objects.create(user=user, **validated_data)
+        return student
 
     def update(self, instance, validated_data):
-        class_name = validated_data.pop("class_name", None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+        user_data = validated_data.pop('user', None)
+        if user_data:
+            instance.user.username = user_data.get('username', instance.user.username)
+            instance.user.email = user_data.get('email', instance.user.email)
+            instance.user.first_name = user_data.get('first_name', instance.user.first_name)
+            instance.user.last_name = user_data.get('last_name', instance.user.last_name)
+            instance.user.save()
+
+        instance.full_name = validated_data.get('full_name', instance.full_name)
         instance.save()
-
-        if class_name is not None:
-            instance.tracks.all().delete()
-            for class_date in class_name:
-                Student.objects.create(album=instance, **class_date)
-
         return instance
 
 
-class TeacherSerializers(serializers.ModelSerializer):
+class ClassSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Teacher
-        fields = "__all__"
+        model = Class
+        fields = ['cla_ss']
